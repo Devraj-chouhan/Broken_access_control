aef2c0d6b71782f4de7d808019d4c6678d793364
Devraj <cdev4361@gmail.com>
2025-07-14 11:32:50 +0530

    Updated script.js with correct backend URL

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").onsubmit = async (e) => {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const response = await fetch("http://127.0.0.1:5000/login", {
+  const response = await fetch("https://broken-access-control.onrender.com/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
@@ -12,11 +12,10 @@ document.getElementById("loginForm").onsubmit = async (e) => {
 
   const result = await response.json();
   if (result.success) {
-  localStorage.setItem("token", result.token);
-  localStorage.setItem("username", username);
-  window.location.href = result.redirect;
-}
- else {
+    localStorage.setItem("token", result.token);
+    localStorage.setItem("username", username);
+    window.location.href = result.redirect;
+  } else {
     document.getElementById("message").innerText = result.message;
   }
 };
